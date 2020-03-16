1719c5842554e980c4313ca06bc933f2ff89167a
PeggyBrown <ola.qnysz@gmail.com>
2020-03-16 18:56:00 +0100

    Project setup

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    public void shouldReturnZeroWhenGotEmptyString() {
+        assertThat(true).isEqualTo(true); // xD
+    }
+}
