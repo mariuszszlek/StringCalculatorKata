d793a56d01b63673430600dec5ac7a1359bdd84d
mariuszszlek <mariuszszlek@gmail.com>
2022-12-18 11:13:30 +0100

    reset project - learn TDD String calculator Kata

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,3 @@
 public class Calculator {
-    public int add(String numbers) {
-        if ("".equals(numbers))
-            return 0;
-        else {
-            int result = 0;
-            for (String number : numbers.split(",")) {
-                result += getIntFrom(number);
-            }
-            return result;
-        }
-    }
 
-    private int getIntFrom(String individualNumber) {
-        return Integer.parseInt(individualNumber);
-    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,40 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
-
-    @BeforeEach
-    void initCalculator() {
-        calculator = new Calculator();
-    }
 
     @Test
-    public void shouldReturnZeroWhenEmptyStringGiven() {
-        int result = calculator.add("");
-        assertThat(result).isEqualTo(0);
+    public void shouldReturnZeroWhenGotEmptyString() {
+        assertThat(true).isEqualTo(true);
     }
 
-    @Test
-    void shouldReturnOneWhenOneGiven() {
-        int result = calculator.add("1");
-        assertThat(result).isEqualTo(1);
-    }
-
-    @Test
-    void shouldReturnTwoWhenTwoGiven() {
-        int result = calculator.add("2");
-        assertThat(result).isEqualTo(2);
-    }
-
-    @Test
-    void shouldReturnSumOfTwoNumbers() {
-        int result = calculator.add("1,2");
-        assertThat(result).isEqualTo(3);
-    }
-
-    @Test
-    void shouldReturnSumOfThreeNumbers() {
-        int result = calculator.add("1,2,3");
-        assertThat(result).isEqualTo(6);
-    }
 }
